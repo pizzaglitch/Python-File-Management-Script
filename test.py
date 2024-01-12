9745700c7d55b3c6a079d9fdc1cb4530a537abda
Garrett Hufstedler <garretthufstedler@gmail.com>
2024-01-11 21:15:26 -0500

    Beginning refactor. Added cleaner file splitting option to target file names.

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import os
 import shutil
-import re
+
 # To-Do
 
 # Problem
@@ -18,20 +18,22 @@ import re
 #LRAG2311001 (7 digit name/yy/mm/dd)
 #Artist first initial (L), First three letters of last name (RAG)
 #Year (23), Month (1), Day (1)
-#Artwork num (001)
+#Artwork num (001)p
 #f = open("test.txt", "r")
 
 
 #1st push: script works when all files are in same directory as test.py
 #2nd push: works when all files are organized as they arrive from the server
-#To do: Reduce repetitive code, check for edge cases
+    #To do: Reduce repetitive code, check for edge cases 
+        #(instead of matching by string length, match by exact phrasing (case insensitive))
+        #could use split, then index[1] to target actual file name instead of checking substr
+
 
 path = os.path.join('/Users/garebear/Desktop/Python Test')
 jpgPath = os.path.join('/Users/garebear/Desktop/Python Test', 'JPG')
 tiffPath = os.path.join('/Users/garebear/Desktop/Python Test', 'TIFF')
-directory = '/Users/garebear/Desktop/Python Test'
 
-#test paths for navigating through folders in the format they arrive in
+#test paths for navigating through folders in the format they arrive in (Push #2)
 testSmallJpgPath = os.path.join(path, 'JPEG-2000-under2MB')
 testLargeJpgPath = os.path.join(path, 'JPEG-4800-under7MB')
 testTiffPath = os.path.join(path, 'TIFF')
@@ -40,12 +42,59 @@ testTiffPath = os.path.join(path, 'TIFF')
 maxSmallFileSize = 2000000
 maxLargeFileSize = 7100000
 
-#Trial code for solution based on proper downloaded file format. Works. 
+#Trial 3 (Remove repetition, fix edge cases)
+for file in os.scandir(path):
+    fileNameString = str(file)[:-1] #convert file name to string
+    splitStr = fileNameString.split()
+    imgFolder = splitStr[1] #JPEG-2000-under2MB
+    #print(imgFolder)
+    if imgFolder == 'JPEG-2000-under2MB':
+       print("hi")
+       #for subFile in os.scandir(testSmallJpgPath):
+        #   print(subFile)
+
+
+    #for subFolder in os.scandir(path):
+    """ for subFile in os.scandir(testSmallJpgPath):
+        artistInventoryNumber = str(subFile)[11:22] #snip off inventory num of filename 
+        inventoryNumberFolder = os.path.join(path, artistInventoryNumber)
+        ext = os.path.splitext(subFile)[-1].lower() #get file ext
+        fileSize = os.path.getsize(subFile) #get file size
+
+        if not os.path.exists(inventoryNumberFolder):
+            os.makedirs(inventoryNumberFolder)
+
+        lowResJPGFolder = os.path.join(path, artistInventoryNumber, 'Low Res')
+        if not os.path.exists(lowResJPGFolder): 
+            os.makedirs(lowResJPGFolder)
+
+        highResJPGFolder = os.path.join(path, artistInventoryNumber, 'High Res')
+        if not os.path.exists(highResJPGFolder):
+            os.makedirs(highResJPGFolder)
+        
+        tiffFolder = os.path.join(path, artistInventoryNumber, 'TIFF')
+        if not os.path.exists(tiffFolder):
+            os.makedirs(tiffFolder)
+        if fileSizeFolder == 'JPEG-2000-under2MB':
+            for subFile in os.scandir(testSmallJpgPath):
+                shutil.move(file, lowResJPGFolder)
+
+    if fileSizeFolder == 'JPEG-4800-under7MB':
+        for subFile in os.scandir(testLargeJpgPath):
+            shutil.move(file, highResJPGFolder)
+
+    if fileSizeFolder == 'TIFF':
+        for subFile in os.scandir(testTiffPath):
+            shutil.move(file, tiffFolder)  """
+######
+######
+#Trial code for solution based on proper downloaded file format. Works. Need to reduce repetitive code.
+#Push #2
+"""
 for file in os.scandir(path):
     fileNameString = str(file) #convert file name to string
     fileSizeFolder = fileNameString[11:29] #JPEG-2000-under2MB
     tiffSizeFolder = fileNameString[11:15]
-    print (tiffSizeFolder)
     #for subFolder in os.scandir(path):
     if fileSizeFolder == 'JPEG-2000-under2MB':
         for subFile in os.scandir(testSmallJpgPath):
@@ -112,8 +161,9 @@ for file in os.scandir(path):
                 os.makedirs(tiffFolder)
 
             shutil.move(subFile, tiffFolder)
+"""
 
-# Works with all files in same folder. 
+#Push #1: Works only when all files are in same folder.
 """ for file in os.scandir(path):
     ext = os.path.splitext(file)[-1].lower() #get file ext
     fileSize = os.path.getsize(file) #get file size
@@ -147,4 +197,8 @@ for file in os.scandir(path):
         if fileSize > maxSmallFileSize and fileSize <= maxLargeFileSize: #move <7mb files 
             shutil.move(file, highResJPGFolder)
  """
-#f.close()
+
+#Delete old, empty folders when done
+""" os.rmdir(testSmallJpgPath)
+os.rmdir(testLargeJpgPath)
+os.rmdir(testTiffPath) """
